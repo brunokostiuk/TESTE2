1c83c302e4c5692f92101cca514cd6f6dd5ce3b3
Bruno Kostiuk <brunokostiuk@hotmail.com>
2014-10-09 13:32:46 -0300

    Revert "correto"
    This reverts commit 8655befa0e1bb64af467b5f223485ccbc689dda0.

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -15,6 +15,6 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("TESTE");
+        System.out.println("TESTE");;
     }
 }
