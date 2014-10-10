780de7db5d09fcf0eb2ea61b33928a8040769522
Bruno Kostiuk <brunokostiuk@hotmail.com>
2014-10-10 10:39:10 -0300

    erro2

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -13,6 +13,7 @@ public class JavaApplication1 {
     /**
      * @param args the command line arguments
      */
+	private
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("TESTE");
