d4fb7bde839df4e77c7d964c6198f1f6160ea88a
Bruno Kostiuk <brunokostiuk@hotmail.com>
2014-10-14 14:39:52 -0300

    commit

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -17,6 +17,5 @@ public class JavaApplication1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("TESTE");
-		System.out.println("TESTE");
     }
 }
